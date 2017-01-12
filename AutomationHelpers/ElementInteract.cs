c86bae27bb863317fb39ce9ff55c139411db0e68
JamesWalters79 <waltersjameswalters@gmail.com>
2017-01-12 12:06:47 +0000

    Add error handling for cases where element is not there

--- AutomationHelpers/ElementInteract.cs
+++ AutomationHelpers/ElementInteract.cs
@@ -12,7 +12,17 @@ namespace AutomationHelpers
         {
             try
             {
-                 ElementPatterns.GetInvokePattern(ElementFinder.GetElementByNameOrAutomationId(elementNameOrAutomationId, parent)).Invoke();
+                AutomationElement target = ElementFinder.GetElementByNameOrAutomationId(elementNameOrAutomationId, parent);
+
+                if (target != null)
+                {
+                    ElementPatterns.GetInvokePattern(target).Invoke();
+                }
+                else
+                {
+                    throw new ElementNotAvailableException("element with name or automationID " + elementNameOrAutomationId + " could not be found in InvokeElement");
+                }
+
             }
             catch (Exception)
             {
@@ -38,11 +48,21 @@ namespace AutomationHelpers
         {
             try
             {
-                Rect rect = ElementFinder.GetElementByNameOrAutomationId(elementNameOrAutomationId, parent).Current.BoundingRectangle;
-                Mouse.MoveTo(new System.Drawing.Point((int)rect.Left + 10, (int)rect.Bottom - 10));
-                Thread.Sleep(500);
-                Mouse.Click(MouseButton.Left);
-                Thread.Sleep(500);
+                AutomationElement target = ElementFinder.GetElementByNameOrAutomationId(elementNameOrAutomationId, parent);
+
+                if (target != null)
+                {
+                    Rect rect = target.Current.BoundingRectangle;
+                    Mouse.MoveTo(new System.Drawing.Point((int)rect.Left + 10, (int)rect.Bottom - 10));
+                    Thread.Sleep(500);
+                    Mouse.Click(MouseButton.Left);
+                    Thread.Sleep(500);
+                }
+                else
+                {
+                    throw new ElementNotAvailableException("element with name or automationID " + elementNameOrAutomationId + " could not be found in MoveToAndClick");
+                }
+
             }
             catch (Exception)
             {
@@ -53,11 +73,19 @@ namespace AutomationHelpers
         {
             try
             {
-                Rect rect = element.Current.BoundingRectangle;
-                Mouse.MoveTo(new System.Drawing.Point((int)rect.Left + 10, (int)rect.Bottom - 10));
-                Thread.Sleep(500);
-                Mouse.Click(MouseButton.Left);
-                Thread.Sleep(500);
+
+                if (element != null)
+                {
+                    Rect rect = element.Current.BoundingRectangle;
+                    Mouse.MoveTo(new System.Drawing.Point((int)rect.Left + 10, (int)rect.Bottom - 10));
+                    Thread.Sleep(500);
+                    Mouse.Click(MouseButton.Left);
+                    Thread.Sleep(500);
+                }
+                else
+                {
+                    throw new ElementNotAvailableException("element with name or automationID could not be found in MoveToAndClick");
+                }
             }
             catch (Exception)
             {
